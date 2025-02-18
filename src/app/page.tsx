17f32ded5c7369c65d2505fb588c6789892a26c8
EnesKymz <okkeseneskaymaz@proton.me>
2025-02-18 18:47:53 +0300

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,56 @@
 "use client"
-
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useCoinContext } from "./context/pricingContext";
 export default function Home() {
+  const [usdt, setUsdt] = useState<number>();
+
+  const coins = ["bitcoin", "pepe", "ethereum", "sui"]; // Coins
+  const {coinPrices,setCoinPrices}=useCoinContext()
+  useEffect(() => {
+    const fetchCoinPrices = async () => {
+      try {
+        const responses = await Promise.all(
+          coins.map((coin) =>
+            fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`)
+              .then((res) => {
+                if(!res.ok) {
+                  throw new Error(`Failed to fetch price for ${coin}`)
+                }
+                return res.json()
+              })
+              .then((data) => ({
+                coin,
+                price: data[coin]?.usd ? data[coin]?.usd : 0 ,
+              })).catch((error) =>{
+                console.error(error);
+                return {coin, price:0};
+              })
+          )
+        );
+        
+        const prices = responses.reduce((acc, { coin, price  }) => {
+          acc[coin] = price > 0 ? price : 0;
+          return acc;
+        }, {} as { [key: string]: number });
+        localStorage.setItem("prices",JSON.stringify(prices));
+        setCoinPrices(prices);
+      } catch (error) {
+        console.error("Error fetching coin prices:", error);
+      }
+    };
+
+    fetchCoinPrices();
+
+    const intervalId = setInterval(fetchCoinPrices, 30000);
+
+    return () => clearInterval(intervalId);
+  }, [coinPrices,setCoinPrices,coins]);
+
   return (
     <div className="min-h-screen bg-orange-500 flex justify-center items-center p-4">
       <h5>QUANTUM CLUB LFGGGGG</h5>
-    { /* <div className="bg-white shadow-2xl rounded-2xl p-8 w-full max-w-2xl">
+      <div className="bg-white shadow-2xl rounded-2xl p-8 w-full max-w-2xl">
         <div className="text-center mb-6">
           <h2 className="text-2xl font-semibold text-gray-800">How much USDT do you have?</h2>
           <input
@@ -40,7 +86,7 @@ export default function Home() {
             </tbody>
           </table>
         </div>
-      </div>*/}
+      </div>
     </div>
   );
 }
